afa1e3913ac7d2be457b82ba5c43cbb22ae068d1
Jason D'Oyley <jdfdoyley@gmail.com>
2022-10-30 21:16:52 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿/******************************************************************************\
+* Name: Jason D. F. D'Oyley
+* Date: October 30, 2022
+* Assignment CIS214 Performance Assessment - User Entry of Age
+*
+* Main application (program) class
+* Print a line that states "Your Name - Week 4 PA User Entry of Age"
+* Ask the user to enter their age
+* If the age is between 1 and 100
+*   print a message stating the age the user entered
+*   exit the program
+* If the age is less than 1 or greater than 100
+*   generate an ArgumentOutOfRangeException
+*   print the exception information
+*   print an error message
+* If the value is not an integer
+*   handle the FormatException
+*   print the exception information
+*   print an error message
+* Continue asking the user to enter their age until a valid age is entered
+\******************************************************************************/
---
+++ README.md
@@ -0,0 +1,18 @@
+# User Entry of Age
+
+The main application class must meet the following requirements
+
+- Print a line that states "Your Name - Week 4 PA User Entry of Age"
+- Ask the user to enter their age
+- If the age is between 1 and 100
+  - print a message stating the age the user entered
+  - exit the program
+- If the age is less than 1 or greater than 100
+  - generate an ArgumentOutOfRangeException
+  - print the exception information
+  - print an error message
+- If the value is not an integer
+  - handle the FormatException
+  - print the exception information
+  - print an error message
+- Continue asking the user to enter their age until a valid age is entered
